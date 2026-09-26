f4254aec891f31457214ff0f7d3b1326a39eb011
trietbuii <buitriet45@gmail.com>
2026-09-26 21:10:18 +0700

    feat: add app routing for student and organizer pages

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -5,16 +5,27 @@ import EventDetailPage from "../pages/EventDetailPage";
 import TicketPage from "../pages/TicketPage";
 import CheckInPage from "../pages/CheckInPage";
 import DashboardPage from "../pages/DashboardPage";
+import EventManagementPage from "../pages/EventManagementPage";
+import EventFormPage from "../pages/EventFormPage";
+import ParticipantsPage from "../pages/ParticipantsPage";
+import StaffPage from "../pages/StaffPage";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Student routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/events/:eventId" element={<EventDetailPage />} />
         <Route path="/tickets/:ticketId" element={<TicketPage />} />
         <Route path="/check-in" element={<CheckInPage />} />
+
+        {/* Organizer routes */}
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/events" element={<EventManagementPage />} />
+        <Route path="/dashboard/events/new" element={<EventFormPage />} />
+        <Route path="/dashboard/participants" element={<ParticipantsPage />} />
+        <Route path="/dashboard/staff" element={<StaffPage />} />
       </Routes>
     </BrowserRouter>
   );
